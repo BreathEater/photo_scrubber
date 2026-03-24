6fbaaa54d6da700a9db01d96482da1f2196a2595
BreathEater <karolkwork@gmail.com>
2026-03-24 17:54:34 +0000

    Connection to db established, the problem right is probably an error in exiftool, but there's no need to do compose down -v anymore

--- backend/api/router.py
+++ backend/api/router.py
@@ -3,5 +3,5 @@ from api.scrub_post import router as scrub_router
 from api.stats_get import router as stats_router
 
 router = APIRouter()
-router.include_router(stats_router, prefix="/stats")
-router.include_router(scrub_router, prefix="/scrub")
+router.include_router(stats_router)
+router.include_router(scrub_router)
